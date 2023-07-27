187afdc15e8e51a1457e62cd21adf081977474f3
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-07-27 12:08:18 -0300

    feat:add login routes,controller,interface, schema and creatTokenService

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import express from "express";
 import { errorHandler } from "./error";
 import { contactRoutes } from "./routes/contact.routes";
 import { clientRoutes } from "./routes/client.routes";
+import { loginRoutes } from "./routes/login.routes";
 
 
 const app = express();
 app.use(express.json());
+app.use("/login",loginRoutes)
 app.use("/clients", clientRoutes);
 app.use("/contacts", contactRoutes);
 app.use(errorHandler);
