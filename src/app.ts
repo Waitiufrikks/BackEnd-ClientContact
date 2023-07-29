46f08fb7964f5f77d3c6ad668af64614b3b0eeb5
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-07-29 05:15:50 -0300

    feat:add cors to connect any front-end request

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
+import cors from 'cors';
 import { errorHandler } from "./error";
 import { contactRoutes } from "./routes/contact.routes";
 import { clientRoutes } from "./routes/client.routes";
@@ -8,6 +9,7 @@ import { loginRoutes } from "./routes/login.routes";
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/login",loginRoutes)
 app.use("/clients", clientRoutes);
